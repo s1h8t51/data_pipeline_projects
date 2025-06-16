01b728562733a7b635cc3fa40c1ed14f20735cf6
sahitya_gantala <sahityagantalausa@gmail.com>
2025-06-16 14:32:31 -0400

    Update churn_model.py

--- churn_model.py
+++ churn_model.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+import joblib  # to save model
+
+
 
