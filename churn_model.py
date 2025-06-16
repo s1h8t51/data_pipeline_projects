ae7608cd9cf38b13e949cc898d89bb963fc275d3
sahitya_gantala <sahityagantalausa@gmail.com>
2025-06-16 14:33:45 -0400

    Update churn_model.py

--- churn_model.py
+++ churn_model.py
@@ -6,5 +6,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 import joblib  # to save model
 
+def load_data(file_path='data/telco_churn.csv'):
+    return pd.read_csv(file_path)
+
+
 
 
